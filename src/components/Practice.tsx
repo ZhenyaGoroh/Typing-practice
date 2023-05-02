ae12e5c0578502e62cf7c40514b4b27b7f8551a1
ZhenyaGoroh <zhenyagoroh@gmail.com>
2023-05-02 13:43:48 +0300

    cursor

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useRef, useState } from "react"
 import { v4 as uuidv4 } from "uuid"
 import { BiSpaceBar } from "react-icons/bi"
+import { useNavigate } from "react-router-dom"
 import s from "../stylesheet/Practice.module.scss"
 import { useStore } from "../store/store"
 
 function Practice() {
+  const navigate = useNavigate()
+
   const text = useStore((state) => state.text)
 
   const textNode = useRef<HTMLDivElement>(null)
+  const cursorRef = useRef<HTMLSpanElement>(null)
 
   let char = 0
   // let line = 0
@@ -15,7 +19,7 @@ function Practice() {
 
   function increaceChar(): void {
     char += 1
-    if (char === 10) {
+    if (char === text.split(". ")[line].length) {
       setLine((prevLine) => {
         char = 10
         return prevLine + 1
@@ -23,10 +27,46 @@ function Practice() {
     }
   }
 
+  function pushCursor() {
+    if (
+      textNode.current &&
+      textNode.current.children[char + 1] &&
+      cursorRef.current
+    ) {
+      if (char + 1 === 2) {
+        cursorRef.current.style.left = `${
+          parseFloat(cursorRef.current?.style.left) +
+          textNode.current.children[char + 1].getBoundingClientRect().width
+        }px`
+      } else {
+        cursorRef.current.style.left = `${
+          parseFloat(cursorRef.current?.style.left) +
+          textNode.current.children[char + 1].getBoundingClientRect().width
+        }px`
+      }
+    }
+  }
+
   useEffect(() => {
+    if (text.length < 1) {
+      navigate("/")
+    }
+    if (
+      textNode.current &&
+      textNode.current.children[char + 1] &&
+      cursorRef.current
+    ) {
+      cursorRef.current.style.left = `${
+        // parseFloat(cursorRef.current?.style.left) +
+        textNode.current.children[char + 1].getBoundingClientRect().width
+      }px`
+    }
     function handleKeyDown(event: KeyboardEvent) {
-      if (event.key === text.slice(10 * line, 10 * line + 10)[char]) {
-        textNode.current?.children[char].classList.add(`${s.correct}`)
+      if (event.key === text.split(". ")[line][char]) {
+        textNode.current?.children[char + 1].classList.add(`${s.correct}`)
+        if (char + 1 !== text.length) {
+          pushCursor()
+        }
         increaceChar()
       }
     }
@@ -42,9 +82,10 @@ function Practice() {
     <div className="container">
       <div className={s.practice}>
         <div ref={textNode} className={s.practice__text}>
+          <span ref={cursorRef} className={s.cursor} />
           {text
-            .slice(10 * line, 10 * line + 10)
-            .split("")
+            .split(". ")
+            [line].split("")
             .map((ch) => {
               return ch === " " ? (
                 <BiSpaceBar className={s.spaceSpan} key={uuidv4()} />
