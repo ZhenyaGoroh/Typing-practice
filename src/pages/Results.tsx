e496284c047679c86d9266caadca73e7c3161274
ZhenyaGoroh <zhenyagoroh@gmail.com>
2023-05-11 18:12:31 +0300

    empty result array

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -10,55 +10,61 @@ function Results() {
 
   return (
     <div className={s.wrapper}>
-      <div className={s.table}>
-        <div className={s.table__titles}>
-          <span className={s.titles__title}>Order</span>
-          <span className="vr" />
-          <span className={s.titles__title}>WPM</span>
-          <span className="vr" />
-          <span className={s.titles__title}>Time</span>
-          <span className="vr" />
-          <span className={s.titles__title}>Mistakes</span>
+      {results.length > 0 ? (
+        <div className={s.table}>
+          <div className={s.table__titles}>
+            <span className={s.titles__title}>Order</span>
+            <span className="vr" />
+            <span className={s.titles__title}>WPM</span>
+            <span className="vr" />
+            <span className={s.titles__title}>Time</span>
+            <span className="vr" />
+            <span className={s.titles__title}>Mistakes</span>
+          </div>
+          <div className={s.table__results}>
+            {results.map((result, index) => {
+              const resultState: {
+                id: string
+                wpm: number
+                seconds: number
+                minutes: number
+                mistakes: number
+                text: string
+              } = {
+                id: result.id,
+                wpm: result.wpm,
+                seconds: result.seconds,
+                minutes: result.minutes,
+                mistakes: result.mistakes,
+                text: result.text,
+              }
+              return (
+                <Link
+                  className={s.link}
+                  key={uuidv4()}
+                  to={`/results/${result.text.split(" ")[0]}…`}
+                  state={resultState}
+                >
+                  <Result
+                    order={index + 1}
+                    wpm={result.wpm}
+                    seconds={result.seconds}
+                    minutes={result.minutes}
+                    mistakes={result.mistakes}
+                  />
+                </Link>
+              )
+            })}
+          </div>
         </div>
-        <div className={s.table__results}>
-          {results.map((result, index) => {
-            const resultState: {
-              id: string
-              wpm: number
-              seconds: number
-              minutes: number
-              mistakes: number
-              text: string
-            } = {
-              id: result.id,
-              wpm: result.wpm,
-              seconds: result.seconds,
-              minutes: result.minutes,
-              mistakes: result.mistakes,
-              text: result.text,
-            }
-            return (
-              <Link
-                className={s.link}
-                key={uuidv4()}
-                to={`/results/${result.text.split(" ")[0]}…`}
-                state={resultState}
-              >
-                <Result
-                  order={index + 1}
-                  wpm={result.wpm}
-                  seconds={result.seconds}
-                  minutes={result.minutes}
-                  mistakes={result.mistakes}
-                />
-              </Link>
-            )
-          })}
+      ) : (
+        <div className={s.empty}>
+          Looks like you haven&apos;t typed anything yet.
         </div>
-        <Link to="/" className={s.table__link}>
-          Home
-        </Link>
-      </div>
+      )}
+      <Link to="/" className={s.table__link}>
+        Home
+      </Link>
     </div>
   )
 }
